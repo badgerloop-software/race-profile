9ac2c3cad65a2f356527d7fe4cd6b214b30bc0e5
Eric Udlis <udlis@wisc.edu>
2022-09-05 18:55:27 -0500

    Added motor data

--- race_profile/constants.py
+++ race_profile/constants.py
@@ -3,6 +3,7 @@ TIME_RES = .1 # Seconds
 PROFILE_LENGTH = 5 # Seconds
 STARTING_KWH = 6.156 # 100% State of Charge
 FULL_PACK_KWH = 6.156 # kWh
+HV_PACK_VOLTAGE = 96 # Volts
 
 # Non-Configurable Constants
 SECONDS_PER_HOUR = 3600
--- race_profile/run_profile.py
+++ race_profile/run_profile.py
@@ -3,6 +3,8 @@ import numpy as np
 
 from constants import *
 
+import MotorData as motor
+
 def create_run_profile() -> pd.DataFrame:
     df = setup_dataframe() # Always start with this
     return df # Must return a dataframe of the run profile
@@ -30,7 +32,7 @@ def calculate_power_draw(df: pd.DataFrame) -> pd.DataFrame:
     """
     Calculate the power draw of the vehicle
     """
-    df['power_draw'] = 10
+    df['power_draw'] = df['motor_current'] * HV_PACK_VOLTAGE
     return df
 
 def calculate_range(df: pd.DataFrame) -> pd.DataFrame:
@@ -40,11 +42,43 @@ def calculate_range(df: pd.DataFrame) -> pd.DataFrame:
     df['range'] = 0
     return df
 
+def set_motor_current(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Set the motor current of the vehicle (in Amps)
+    """
+    df['motor_current'] = 1
+    return df
+
+def get_motor_torque(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Calculate the motor torque of the vehicle (in Newton-meters)
+    """
+    df['motor_torque'] = motor.calculated_torque_current(df['motor_current'])
+    return df
+
+def get_motor_RPM(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Calculate the motor RPM of the vehicle (in RPM)
+    """
+    df['motor_RPM'] = motor.calculated_RPM_current(df['motor_current'])
+    return df
+
+def calculate_motor_cols(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Calculate the motor columns of the vehicle
+    """
+    df = set_motor_current(df)
+    df = get_motor_torque(df)
+    df = get_motor_RPM(df)
+    return df
+
+
 def main():
     df = create_run_profile()
+    df = calculate_motor_cols(df)
+    # df = calculate_range(df)
     df = calculate_power_draw(df)
     df = calculate_soc(df)
-    df = calculate_range(df)
     print(df)
 
 
