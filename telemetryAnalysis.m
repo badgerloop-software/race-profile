761358982311a575b54794ea6eb8b0d899798550
Jacob <jcpetrie@wisc.edu>
2024-03-17 16:38:19 -0500

    conow copmutes drag coefficient

--- telemetryAnalysis.m
+++ telemetryAnalysis.m
@@ -7,6 +7,10 @@ filename = "Data/TelemetryMS.xlsx";
 DATA = readtable(filename);
 
 mass = 283; % KG
+rho = 1.2; % density of air, kg/m^3, can change with altitude
+A = 1.262; % frontal area, m^2, Source: Ben Colby
+gravity = 9.81; % m/s
+weight = mass * gravity;
 
 %% Compute acceleration at every instant
 times = DATA.Var1; % no column header for the timestamps so it is 'var1' by default
@@ -24,6 +28,16 @@ accel = diff(speeds) ./ diff(times); % TODO: convert from forward to central dif
 force = mass * accel;
 
 % Subtract off rolling resistance
-
+dragForce = force - rollingResistance(speeds(1:(end-1)), weight);
+dragCoeff = dragForce ./ ( 0.5 * rho * A * (speeds(1:(end-1)).^2) );
 
 % Calculate Drag coefficient at each speed
+
+%% Functions
+
+% Model for rolling resistance as a funciton of velocity
+% Source: Ben Colby
+function rr = rollingResistance(speed, normalForce)
+    C_rr = 0.0017 .* exp(0.0054 .* speed);
+    rr = C_rr * normalForce;
+end
