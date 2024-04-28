e5685cb8d4877b7f12d1ba6cda8ca33a84be0f9a
Jacob P <jcpetrie@wisc.edu>
2024-04-27 20:01:55 -0500

    atmospheric properties and organization

--- TestDataAnalysis/FindCoastDown.m
+++ TestDataAnalysis/FindCoastDown.m
@@ -2,25 +2,31 @@
 % Jacob Petrie
 
 function FindCoastDown(inputfilename, outputfilename)
+    % all data from file
     DATA = readtable(inputfilename);
+
+    filetype = ''; % Used later to write data
 
     % Create output Destination for outputs
     folderName = outputfilename;
     if (contains(outputfilename, '.'))
         folderName = extractBefore(outputfilename, '.');
         mkdir(folderName);
-    else 
+        filetype = join(['.', extractAfter(outputfilename, '.')]);
+    else
         mkdir(folderName);
+        filetype = '.csv';
     end
-
+    
     % Identify Coast Down regions
-    isCoasting = (DATA.speed > 10) & (DATA.acceletator_pedal == 0);
-    % !!!!!!!!!!!ADD CONDITION TO ENSURE NOT BREAKING!!!!!!!!!!!!!
+    isCoasting = (DATA.speed > 10) ...              % Car is moving
+               & (DATA.accelerator_pedal == 0)...   % Car is not motoring
+               & (DATA.foot_brake == 0);            % Car is not braking
     
     % filter to coast downs that are of a certain length
     numSeconds = 5;
     unitConversion = 1000; % Timestamps are given in milliseconds
-
+    
     % Find runs of adjacent true values in isCoasting
     differences = diff([false; isCoasting; false]);
     startIndices = find(differences == 1);
@@ -28,25 +34,38 @@ function FindCoastDown(inputfilename, outputfilename)
     
     % Filter isCoasting for runs lasting longer than numSeconds
     isCoastingFiltered = false(size(isCoasting));
+    numCoasts = 0;
     for i = 1:numel(startIndices)
         endTime = DATA{endIndices(i),1};
         startTime = DATA{startIndices(i),1};
+        thisCoastIndices = startIndices(i):endIndices(i);
+        
         if (endTime - startTime)/unitConversion > numSeconds
-            isCoastingFiltered(startIndices(i):endIndices(i)) = true;
+            numCoasts = numCoasts + 1;
+            isCoastingFiltered(thisCoastIndices) = true;
+
+            % Save coast to a file
+            filepath = join([folderName, '/']);
+            name = join([folderName, '(', num2str(numCoasts), ')', filetype]);
+            fullName = join([filepath, name]);
+            writetable(DATA(thisCoastIndices, :), fullName);
         end
+               
     end
 
-    coastData = DATA(isCoasting, :);
-    
+    % coastData = DATA(isCoasting, :);
+    % 
     % while true
     %     % 
     % 
     %     % Populate destination
     %     regionNum = regionNum + 1;
     % end
-
+    % 
+    % figure
     % plot(coastData.Var1, coastData.speed, '-*');
-    plot(isCoastingFiltered)
+    % figure
+    % plot(isCoastingFiltered)
     
-
+    
 end
