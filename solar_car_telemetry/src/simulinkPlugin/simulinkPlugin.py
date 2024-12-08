91e8ef6b478d69b25e347ead09bba698744d57d9
sbasu107 <40325803+sbasu107@users.noreply.github.com>
2024-12-08 13:12:52 -0600

    Working on sending data into SImulink

--- solar_car_telemetry/src/simulinkPlugin/simulinkPlugin.py
+++ solar_car_telemetry/src/simulinkPlugin/simulinkPlugin.py
@@ -1,12 +1,27 @@
 """
-Send data to Simulink and launch the Simulink model.
+Try to connect to Simulink and send data to it through TCP/IP Connection. 
+If the connection is successful, launch the Simulink model.
 """
+import socket, struct
+import matlab.engine as m
+
+TCP_IP = 'localhost'
+TCP_PORT = 30001
+BUFFER_SIZE = 1024
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+print('Waiting for Simulink to start')
+s.listen(1)
+conn, addr = s.accept()
+print('Connection address: ', addr)
+
 
 def launchSimulink():
     """
     Launch the Simulink model.
     """
-    eng = matlab.engine.start_matlab()
+    eng = m.start_matlab()
     eng.sim('Car.slx')
     pass
 
