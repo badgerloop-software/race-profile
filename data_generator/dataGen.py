51e9df066a6df0280705e99c01224d3284d0c243
sbasu107 <sbasu107@gmail.com>
2025-02-16 12:10:23 -0600

    updated data generator, converted raw data to csv

--- data_generator/dataGen.py
+++ data_generator/dataGen.py
@@ -5,13 +5,13 @@ if __name__ == '__main__':
     r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
     data_dict = {}
-    input_data = pd.read_csv("TestDataAnalysis/coasts2024-04-141655.csv")
+    input_data = pd.read_csv("TestDataAnalysis\\2024-4-7rawdata.csv")
     
     for i in range(0, len(input_data)):
         row = input_data.iloc[i]
         data_dict = row.to_dict()
         r.mset(data_dict)
         print(f"Fed row {i}.")
-        time.sleep(0.5)
+        time.sleep(30)
         
     print("Feeding complete. Restart with \"python3 dataGen.py\"")
