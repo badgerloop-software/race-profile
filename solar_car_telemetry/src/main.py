37666471136f115904f366deb5c8523344f04254
sbasu107 <sbasu107@gmail.com>
2025-02-17 12:59:23 -0600

    Added error handling box for live data plotting.

--- solar_car_telemetry/src/main.py
+++ solar_car_telemetry/src/main.py
@@ -7,6 +7,8 @@ import redis, config
 import numpy as np
 #import matlab
 import pandas as pd
+import time
+import ctypes
 
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -23,36 +25,54 @@ fig, ax = plt.subplots()
 xs = []  # Store timestamps
 ys = []  # Store Var1 values
 
-def animate(i):
-    global xs, ys
+def animate(i, telem_var = 'Var1'):
+    try:
+        global xs, ys
 
-    # Get Var1 value
-    df = extractVars.save_variables()
-    Var1 = df.loc[df['telem_variables'] == 'Var1'].values[0][1]
-    
-    # Add x and y to lists
-    xs.append(datetime.now())
-    ys.append(Var1)
-    
-    # Limit lists to 50 items
-    xs = xs[-50:]
-    ys = ys[-50:]
-    
-    # Clear axis
-    ax.clear()
-    
-    # Plot data
-    ax.plot(xs, ys)
-    
-    # Format plot
-    plt.xticks(rotation=45, ha='right')
-    plt.subplots_adjust(bottom=0.30)
-    plt.title('Var1 Over Time')
-    plt.ylabel('Var1 Value')
+        # Get Var1 value
+        df = extractVars.save_variables()
+        Var1 = df.loc[df['telem_variables'] == telem_var].values[0][1]
+        
+        # Add x and y to lists
+        xs.append(datetime.now())
+        ys.append(Var1)
+        
+        # Limit lists to 50 items
+        xs = xs[-50:]
+        ys = ys[-50:]
+        
+        # Clear axis
+        ax.clear()
+        
+        # Plot data
+        ax.plot(xs, ys)
+        
+        # Format plot
+        plt.xticks(rotation=45, ha='right')
+        plt.subplots_adjust(bottom=0.30)
+        plt.title('Var1 Over Time')
+        plt.ylabel('Var1 Value')
+
+    except AttributeError as e:
+        plt.close('all')
+
+        #Error Box
+        ctypes.windll.user32.MessageBoxW(0, "Please turn on Data Generator before plotting live data.", "Data Collection Error", 0)
+        ##  Styles(Last Number):
+        ##  0 : OK
+        ##  1 : OK | Cancel
+        ##  2 : Abort | Retry | Ignore
+        ##  3 : Yes | No | Cancel
+        ##  4 : Yes | No
+        ##  5 : Retry | Cancel 
+        ##  6 : Cancel | Try Again | Continue
 
 
 if __name__ == "__main__":
-    weather_data = get_weather_data()
-    # Set up plot to call animate() function periodically
-    ani = animation.FuncAnimation(fig, animate, interval=1000)
-    plt.show()
+    show_animation = False
+    if show_animation is True:
+        # Set up plot to call animate() function periodically
+        ani = animation.FuncAnimation(fig, animate, interval=1000)
+        plt.show()
+    
+    print("Finished.")
